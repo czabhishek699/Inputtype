87da5368116f05340f22c5e14029d6e0182beb46
AVISHEK OJHA <140743024+czabhishek699@users.noreply.github.com>
2023-09-09 15:51:51 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const masterPassword = "your_master_password"; // Replace with your master password
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+    const enteredPassword = document.getElementById("password").value;
+    if (enteredPassword === masterPassword) {
+        // Password is correct, allow access
+        window.location.href = "welcome.html"; // Redirect to a welcome page
+    } else {
+        // Password is incorrect, display error message
+        document.getElementById("errorMessage").style.display = "block";
+    }
+});￼Enter
